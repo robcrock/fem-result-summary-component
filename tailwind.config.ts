a4a21e04108e98f6376dcf6bdec32e2a26e731eb
Robert Crocker <robcrock@Roberts-Laptop.local>
2024-01-16 07:12:59 -0600

    My first commit.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,49 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
     extend: {
+      colors: {
+        primary: {
+          "light-red": "hsl(0,100%, 67%)",
+          "orangey-yellow": "hsl(39,100%, 56%)",
+          "green-teal": "hsl(166,100%, 37%)",
+          "cobalt-blue": "hsl(234,85%, 45%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          "pale-blue": "hsl(221, 100%, 96%)",
+          "light-lavender": "hsl(241, 100%, 89%)",
+          "dark-gray-blue": "hsl(224, 30%, 27%)",
+        },
+        gradient: {
+          "light-slate-blue": "hsl(252, 100%, 67%)",
+          "light-royal-blue": "hsl(241, 81%, 54%)",
+          "violet-blue": "hsla(256, 72%, 46%, 1)",
+          "persian-blue": "hsla(241, 72%, 46%, 0)",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontSize: {
+        paragraph: "18px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
